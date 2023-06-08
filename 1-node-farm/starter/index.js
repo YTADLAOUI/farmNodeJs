b07810b77d0fc0e9c66748d9844172623b60c45a
youssef tadlaoui <usftadlaoui@gmail.com>
2023-06-08 02:00:09 +0100

    affichage data

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -15,17 +15,35 @@ const url =require('url');
 // fs.writeFile('./txt/avocat.txt',data,err=>{if(err){console.error('error')}})
 //  })
 
-const card= fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+
+ //////////////////////////////////////////////////////////////////////////
+const replaceTamplate= (temp,product)=>{
+  let output= temp.replace(/{%PROUDACTNAME%}/g,product.productName);
+  output=output.replace(/{%IMAG%}/g,product.image);
+  output=output.replace(/{%QUANTITY%}/g,product.quantity);
+  output=output.replace(/{%PROUDACTNUTRIENTSNAME%}/g,product.nutrients);
+  output=output.replace(/{%PRICE%}/g,product.price);
+  output=output.replace(/{%DESCRIPTION%}/g,product.description);
+  output=output.replace(/{%FROM%}/g,product.from);
+  output=output.replace(/{%ID%}/g,product.id);
+ if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+ return output
+}
+const tempCard= fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const overview= fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const product= fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 const data= fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
-// const Jdata=JSON.parse(data)
- //////////////////////////////////////////////////////////////////////////
+ const Jdata=JSON.parse(data)
+
  const server = http.createServer((req,res)=>{
    const namePath = req.url
 if(namePath==='/' || namePath == '/overview'){
+
   res.writeHead(200,{'content-Type': 'text/html'})
-  res.end(overview)
+  const cardsHtml= Jdata.map(el => replaceTamplate(tempCard,el)).join('   ')
+  console.log(cardsHtml)
+   const output= overview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+  res.end(output)
 }else if (namePath =='/pro') {
   res.end("Hello from server Product")
 } else if(namePath===('/api')){
